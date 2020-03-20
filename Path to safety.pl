d6ccd2c5dfa9fae43f0d9c99d2c6073591580ecf
BouSenna <49456936+BouSenna@users.noreply.github.com>
2020-03-20 04:07:50 +0200

    defining all the possible moves.

---
+++ Path to safety.pl
@@ -0,0 +1,19 @@
+% Move up 
+move([X, Y], [New_X, Y]):-
+ X \= 0,
+ New_X is X - 1.
+
+% Move down 
+move([X, Y], [New_X, Y]):-
+ X \= 3,
+ New_X is X + 1.
+
+% Move left 
+move([X, Y], [X, New_Y]):-
+ Y \= 0,
+ New_Y is Y - 1.
+
+% Move right
+move([X, Y], [X, New_Y]):-
+ Y \= 3,
+ New_Y is Y + 1.
