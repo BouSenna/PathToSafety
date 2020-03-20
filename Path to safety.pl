94549ff35cdb44d9e1db9231849de01b892c48ed
BouSenna <49456936+BouSenna@users.noreply.github.com>
2020-03-20 04:53:45 +0200

    Modified the board dimensions to be of variable length and width.

--- Path to safety.pl
+++ Path to safety.pl
@@ -1,21 +1,21 @@
 % Move up 
-move([X, Y], [New_X, Y]):-
+move([X, Y], [New_X, Y], _, _):-
  X \= 0,
  New_X is X - 1.
 
 % Move down 
-move([X, Y], [New_X, Y]):-
- X \= 3,
+move([X, Y], [New_X, Y], L, _):-
+ X \= L,
  New_X is X + 1.
 
 % Move left 
-move([X, Y], [X, New_Y]):-
+move([X, Y], [X, New_Y], _, _):-
  Y \= 0,
  New_Y is Y - 1.
 
 % Move right
-move([X, Y], [X, New_Y]):-
- Y \= 3,
+move([X, Y], [X, New_Y], _, W):-
+ Y \= W,
  New_Y is Y + 1.
 
 prohibitable_move([X, Y]):-
@@ -34,10 +34,10 @@ reverse_list([H|T], R, Temp) :-
   reverse_list(T, R, [H|Temp]). 
 
 
-find_path(Position, Position, Path, Path, Stars, Stars).
+find_path(Position, Position, _, _, Path, Path, Stars, Stars).
 
-find_path([X,Y], End, Visited, Path, CurrentStars, Stars):-
-   move([X, Y], New_Position),
+find_path([X,Y], End, L, W, Visited, Path, CurrentStars, Stars):-
+   move([X, Y], New_Position, L, W),
    \+ prohibitable_move(New_Position),
    \+ member(New_Position, Visited),
    (preferable_move(New_Position) -> CurrentStars1 is CurrentStars + 1 ;
@@ -47,5 +47,8 @@ find_path([X,Y], End, Visited, Path, CurrentStars, Stars):-
 path_to_safety(Moves, Stars):-
    start_Position(St),
    end_Position(End),
-   find_path(St, End, [St], Reversed_Moves, 0, Stars),
+   dim(L, W),
+   Length = L - 1,
+   Width = W - 1,
+   find_path(St, End, Length, Width, [St], Reversed_Moves, 0, Stars),
    reverse_list(Reversed_Moves, Moves).
